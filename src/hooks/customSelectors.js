2d71030bbae1e5d11cc3badaae5471969f51367e
Ailton Souza <ailton1234@hotmail.com>
2021-03-08 22:52:58 -0300

    Correção dos filtros

--- src/hooks/customSelectors.js
+++ src/hooks/customSelectors.js
@@ -1,9 +1,9 @@
 const customSelector = () => {
   const mathSimbols = [
-    { Simbolo: "<", valor: "Less than" },
-    { Simbolo: ">", valor: "Bigger than" },
-    { Simbolo: "≤", valor: "Less than or equal to" },
-    { Simbolo: "≥", valor: "Bigger than or equal to" },
+    { Simbolo: "<", valor: "Bigger than" },
+    { Simbolo: ">", valor: "Less than" },
+    { Simbolo: "≤", valor: "Bigger than or equal to" },
+    { Simbolo: "≥", valor: "Less than or equal to" },
     { Simbolo: "=", valor: "Equal to" },
     { Simbolo: "≠", valor: "Different to" },
   ];
